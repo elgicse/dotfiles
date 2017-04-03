d79e9f2c05c78ae7bb0989521bd59387f660fd58
Elena Graverini <elena.graverini@cern.ch>
2017-04-03 16:19:41 +0200

    [bin] Add middle-click 3-fingers touchpad configuration

---
+++ .bin/touchpad_settings.sh
@@ -0,0 +1,4 @@
+# Configure the touchpad middle click with three fingers
+# From http://askubuntu.com/questions/130393/how-to-configure-the-touchpad-middle-click
+# Run: gsettings set org.gnome.settings-daemon.peripherals.input-devices hotplug-command "/home/elena/.bin/touchpad_settings.sh"
+synclient TapButton3=2
